2581226ec12170a88c3bd09cbbee6077a2feb9a5
Thilo Fischer <thif@gmx.de>
2023-02-15 23:57:20 +0100

    bugfixes

--- start.sh
+++ start.sh
@@ -3,11 +3,11 @@
 # Copyright (c) 2023  Thilo Fischer.
 # Free software licensed under GPL v3. See LICENSE.txt for details.
 
-date +%T >> context.log
+date >> context.log
 ip address >> context.log
 ip route >> context.log
 
 ip -ts monitor all label 2>&1 | tee -a ip-monitor.log &
 
-./inetconlog.rb 2>&1 | tee -a inetconlog.rb
+./inetconlog.rb 2>&1 | tee -a inetconlog.log
 
